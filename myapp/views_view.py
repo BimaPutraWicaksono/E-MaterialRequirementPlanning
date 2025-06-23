fbf1a0a91551b8d59a850f37f32c82608cb933ac
BimaPutraW08 <bpwicaksono768@gmail.com>
2025-06-23 09:37:54 +0700

    grf-app

--- myapp/views_view.py
+++ myapp/views_view.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from myapp.models import PurchaseRequest
 
-#  home
 @login_required()
 def home(request):
-    return render(request, 'home.html')
+    requests = PurchaseRequest.objects.all()
 
+    count_not_processed = requests.filter(approve_spv__isnull=True).count()
+    count_spv = requests.filter(approve_spv=True, approve_sspv__isnull=True).count()
+    count_sspv = requests.filter(approve_sspv=True, approve_manager__isnull=True).count()
+    count_manager = requests.filter(approve_manager=True, approve_factory_manager__isnull=True).count()
+    count_factory_manager = requests.filter(approve_factory_manager=True).count()
+
+    context = {
+        'chart_data': {
+            'labels': [
+                'Belum Diproses',
+                'SPV',
+                'SSPV',
+                'Manager',
+                'Factory Manager',
+            ],
+            'counts': [
+                count_not_processed,
+                count_spv,
+                count_sspv,
+                count_manager,
+                count_factory_manager,
+            ]
+        }
+    }
+    return render(request, 'home.html', context)
