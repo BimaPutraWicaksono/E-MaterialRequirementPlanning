4a59ad880dabf06a35fde986b92e6527d3f38e3f
BimaPutraW08 <bpwicaksono768@gmail.com>
2025-07-05 11:33:21 +0700

    ngrok

--- myproject/urls.py
+++ myproject/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('e-mrp/', include('myapp.urls')),
+    path('', lambda request: redirect('e-mrp/')),  # Redirect root ke e-mrp
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
